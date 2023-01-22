d4144b6d4602b7c19cacd0b318e402c7a21af85c
Stelios Rammos <stelio.rammos@gmail.com>
2023-01-22 15:13:48 +0100

    v1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import './types';
-
 import { isValidLightningAddress, isValidService } from './validations';
 
+export * from './types';
+export * from './errors';
 export { isValidLightningAddress, isValidService };
 
 export default {
